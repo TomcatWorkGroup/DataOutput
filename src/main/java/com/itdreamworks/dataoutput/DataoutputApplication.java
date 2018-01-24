721007d5ddbcf74bd5bb9940eceed5d86abd2da2
Administrator <80201288@qq.com>
2018-01-24 09:52:17 +0800

    init project

---
+++ src/main/java/com/itdreamworks/dataoutput/DataoutputApplication.java
@@ -0,0 +1,12 @@
+package com.itdreamworks.dataoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataoutputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataoutputApplication.class, args);
+	}
+}
---
+++ src/test/java/com/itdreamworks/dataoutput/DataoutputApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itdreamworks.dataoutput;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataoutputApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
